fc85ebe656f9e215914842ea130926f5d24d408d
HALOGENMAN <Shayak.malakar.159@gmail.com>
2019-10-26 22:30:12 +0530

    image field

--- post/models.py
+++ post/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.urls import  reverse
+import os
 
-# Create your models here.
+def upload_location(instance,filename):#this is also new ,,read it
+    return os.path.join("photos",str(instance.pk),filename)
+
 class Post(models.Model):
     title = models.CharField(max_length=120)
+    #image = models.FileField(null=True,blank=True)
+    image = models.ImageField(upload_to=upload_location,null=True,blank=True,width_field="width_f",height_field="height_f") # this thing is need to be readed both are going same thing
+    width_f = models.IntegerField(default=0)# not necrssary to give user input
+    height_f = models.IntegerField(default=0)# not necesary to give user input
     content = models.TextField()
     update = models.DateTimeField(auto_now=True,auto_now_add=False)
     timestap = models.DateTimeField(auto_now=False,auto_now_add=True)
--- post/views.py
+++ post/views.py
@@ -8,7 +8,7 @@ from django.core.paginator import Paginator
 # Create your views here.
 def post_update(request,id=None):
     instance = get_object_or_404(Post,id=id)
-    form = PostForms(request.POST or None,instance=instance)#it is use for updating 
+    form = PostForms(request.POST or None,request.FILES or None,instance=instance)#it is use for updating 
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -29,7 +29,7 @@ def post_details(request,id=None):
     return render(request,"post_details.html",context)
 
 def post_create(request):
-    form = PostForms(request.POST or None)
+    form = PostForms(request.POST or None,request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         print(form.cleaned_data.get("title"))
